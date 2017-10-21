736b73774c8ef357d0560d03bd59e9e3f70da19b
Karan Acharekar <karancr7acharekar@gmail.com>
2017-10-21 18:00:07 -0400

    Routing table recursive functions

--- pastrytest/lib/pastrytest.ex
+++ pastrytest/lib/pastrytest.ex
@@ -7,7 +7,7 @@ def main(args) do
 
 
     list = getNodeList(input_val)
-    node_id = "0"
+    node_id = "000"
     #IO.inspect list
 
     IO.puts "file hash :: " <> getFileHash("keyur file")
@@ -18,7 +18,7 @@ def main(args) do
 
   def getBitCount do
 
-    bitCount = 4
+    bitCount = 12
 
     bitCount 
 
@@ -139,53 +139,101 @@ def main(args) do
     route_table = Map.put(route_table,rows,[])
     choice = ["A","B","C","D","E","F","0","1","2","3","4","5","6","7","8","9"]
     #IO.inspect route_table  
-    if rows != 0 do
-    IO.puts "helooooooooo"
-    Enum.each nodelist, fn x ->
-    choice = List.delete(choice,String.at(nodeid,rows))
-    if String.slice(x,0..rows-1) == String.slice(nodeid,0..rows-1) do
-      char = String.at(x,rows)
-      if Enum.member?(choice,char) do
-        list = Map.get(route_table,rows)
-        list = list ++ [x]
-        route_table = Map.put(route_table,rows,x)
-        choice = List.delete(choice,char)
-      end
-    end
-    end
-    
+        if rows != 0 do
+        route_table_new = generator123(nodeid,nodelist,route_table,rows,choice)  
+        route_table_new=iter(route_table_new,rows-1,Enum.shuffle(nodelist),nodeid) 
+        route_table_new
 
-    else
-    IO.inspect rows
-    IO.inspect route_table
-    IO.puts "karannnnnnn"
-    Enum.each nodelist, fn x ->
-    choice = List.delete(choice,String.at(x,rows))
+        else
+        IO.inspect rows
+        IO.inspect route_table
+        IO.puts "karannnnnnn"
+
+
+        route_table_new = generator0(nodeid,nodelist,route_table,rows,choice)  
+        route_table_new=iter(route_table_new,rows-1,Enum.shuffle(nodelist),nodeid) 
+        route_table_new
+    end
+  end
+  end
+
+
+
+  
+  def generator0(nodeid,nodelist,route_table,rows,choice) do
+    #IO.inspect route_table
+    if length(nodelist) !=0 do
+    [x|nodelistrest] = nodelist
+    #IO.puts "----"
+    #IO.inspect String.at(x,rows)
+    choice = List.delete(choice,String.at(nodeid,rows))
     #IO.inspect choice
     if String.first(x) != String.first(nodeid) do
-      IO.inspect choice
+      #IO.inspect choice
       char = String.first(x)
+      #IO.puts char
+      #IO.inspect choice
       if Enum.member?(choice,char) do
+        #Io.puts "thissss"
         list = Map.get(route_table,rows)
         list = list ++ [x]
         route_table = Map.put(route_table,rows,list)
-        IO.inspect route_table
+        #IO.inspect route_table
         choice = List.delete(choice,char)
       end  
     end
-    end
-    iter(route_table,rows-1,Enum.shuffle(nodelist),nodeid) 
-    route_table
-  end
-  end
-  end
 
-  
+    generator0(nodeid,nodelistrest,route_table,rows,choice) 
+
+    else
+    IO.puts "heloo"
+    IO.inspect route_table
+
+    route_table
+    end
+    end
+
+
+    def generator123(nodeid,nodelist,route_table,rows,choice) do
+    #IO.inspect route_table
+    if length(nodelist) !=0 do
+    [x|nodelistrest] = nodelist
+    #IO.puts "----"
+    #IO.inspect String.at(x,rows)
+    choice = List.delete(choice,String.at(nodeid,rows))
+    #IO.inspect choice
+    if String.slice(x,0..rows-1) == String.slice(nodeid,0..rows-1) do
+      #IO.inspect choice
+      char = String.at(x,rows)
+      #IO.puts char
+      #IO.inspect choice
+      if Enum.member?(choice,char) do
+        #Io.puts "thissss"
+        list = Map.get(route_table,rows)
+        list = list ++ [x]
+        route_table = Map.put(route_table,rows,list)
+        #IO.inspect route_table
+        choice = List.delete(choice,char)
+      end  
+    end
+
+    generator123(nodeid,nodelistrest,route_table,rows,choice) 
+
+    else
+    IO.puts "heloo"
+    IO.inspect route_table
+
+    route_table
+    end
+    end
+
 
 
   def generate_routing_table(numnodes,nodeid,nodelist) do
     rows = round(Float.ceil(:math.log(numnodes)/:math.log(16))) 
     route_table = %{}
+    IO.puts "---------------"
+    IO.puts rows
     fin_route_table = iter(route_table,rows-1,nodelist,nodeid)  
     IO.puts "here"
     IO.inspect fin_route_table
